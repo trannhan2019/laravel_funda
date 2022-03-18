84da8d14895d01a9407421996543c0945eed37cb
trannhan2019 <53633928+trannhan2019@users.noreply.github.com>
2022-03-18 12:29:42 +0700

    test loi

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -10,30 +10,31 @@ use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    public function register(Request $request){
-        $validator = Validator::make($request->all(),[
-            'name'=>'required|max:191',
-            'email'=>'required|email|max:191|unique:users,email',
-            'password'=>'required|min:8',
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:191',
+            'email' => 'required|email|max:191|unique:users,email',
+            'password' => 'required|min:8',
         ]);
 
-        if ($validator->fails()){
+        if ($validator->fails()) {
             return response()->json([
-                'validation_errors'=>$validator->messages(),
+                'validation_errors' => $validator->messages(),
             ]);
-        }else{
+        } else {
             $user = User::create([
-                'name'=>$request->name,
-                'email'=>$request->email,
-                'password'=>Hash::make($request->password),
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
             ]);
 
-            $token = $user->createToken($user->email.'_Token')->plainTextToken;
+            $token = $user->createToken($user->email . '_Token')->plainTextToken;
             return response()->json([
-                'status'=>200,
-                'username'=>$user->name,
-                'token'=>$token,
-                'message'=>'Register Successfully'
+                'status' => 200,
+                'username' => $user->name,
+                'token' => $token,
+                'message' => 'Register Successfully'
             ]);
         }
     }
