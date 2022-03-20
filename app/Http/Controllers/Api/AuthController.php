3436bd3c7f555b45edcc19c6b54069e129ee2f1e
TranNhan <trannhan.s3@gmail.com>
2022-03-20 20:52:24 +0700

    20.03

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -12,29 +12,92 @@ class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|max:191',
-            'email' => 'required|email|max:191|unique:users,email',
-            'password' => 'required|min:8',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json([
-                'validation_errors' => $validator->messages(),
-            ]);
-        } else {
-            $user = User::create([
-                'name' => $request->name,
-                'email' => $request->email,
-                'password' => Hash::make($request->password),
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|max:191',
+                'email' => 'required|email|max:191|unique:users,email',
+                'password' => 'required|min:8',
             ]);
 
-            $token = $user->createToken($user->email . '_Token')->plainTextToken;
+            if ($validator->fails()) {
+                return response()->json([
+                    'validation_errors' => $validator->messages(),
+                ]);
+            } else {
+                $user = User::create([
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'password' => Hash::make($request->password),
+                ]);
+
+                $token = $user->createToken($user->email . '_Token')->plainTextToken;
+                return response()->json([
+                    'status' => 200,
+                    'username' => $user->name,
+                    'token' => $token,
+                    'message' => 'Register Successfully'
+                ]);
+            }
+        }catch (Exception $error){
             return response()->json([
-                'status' => 200,
-                'username' => $user->name,
-                'token' => $token,
-                'message' => 'Register Successfully'
+                'status' => 500,
+                'message' => 'Error in Registration',
+                'error' => $error,
+            ]);
+        }
+    }
+
+    public function login(Request $request){
+        try {
+            $validator = Validator::make($request->all(),[
+                'email' => 'required|email',
+                'password' => 'required',
+            ]);
+
+            if ($validator->fails()){
+                return response()->json([
+                    'validation_errors' => $validator->messages(),
+                ]);
+            }else{
+                $user = User::where('email', $request->email)->first();
+
+                if (! $user || ! Hash::check($request->password, $user->password)) {
+                    return response()->json([
+                        'status' => 401,
+                        'message' => 'Invalid Credentials',
+                    ]);
+                }else{
+                    $token = $user->createToken($user->email . '_Token')->plainTextToken;
+                    return response()->json([
+                        'status' => 200,
+                        'username' => $user->name,
+                        'token' => $token,
+                        'message' => 'Login Successfully'
+                    ]);
+                }
+            }
+        }catch(Exception $error){
+            return response()->json([
+                'status_code' => 500,
+                'message' => 'Error in Login',
+                'error' => $error,
+            ]);
+        }
+
+    }
+
+    public function logout(){
+        try {
+            auth()->user()->tokens()->delete();
+            return response()->json([
+                'status'=>200,
+                'message'=>'Logged Out Successfully',
+            ]);
+        }catch(Exception $error){
+            return response()->json([
+                'status_code' => 500,
+                'message' => 'Error in Logout',
+                'error' => $error,
             ]);
         }
     }
--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,13 @@ use App\Http\Controllers\Api\AuthController;
 */
 Route::post('register', [AuthController::class, 'register'])
     ->name('register');
+Route::post('login', [AuthController::class, 'login'])
+    ->name('login');
+
+Route::middleware(['auth:sanctum'])->group(function (){
+    Route::post('logout',[AuthController::class,'logout'])
+        ->name('logout');
+});
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
